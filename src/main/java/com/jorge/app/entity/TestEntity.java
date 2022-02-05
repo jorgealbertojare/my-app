b146fbf58537414881497e8f719b6c03a0b9c84f
jorgealbertojare <jorgealbertojare@hotmail.com>
2022-02-05 12:54:06 -0600

    Nombre del proyecto: my-app Descripcion: Version final my app

---
+++ src/main/java/com/jorge/app/entity/TestEntity.java
@@ -0,0 +1,18 @@
+package com.jorge.app.entity;
+
+/**
+ * @author rodriguezja
+ *
+ */
+public class TestEntity 
+{
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
